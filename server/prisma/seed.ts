9a436ebe1b235f85cd9aad660fabb54ba1a4422f
Bruno Lopes <brandao_lopes@live.com>
2022-11-06 00:16:57 -0300

    layout web finalizado - rotas e db finalizados para app web

---
+++ server/prisma/seed.ts
@@ -0,0 +1,64 @@
+import { PrismaClient } from '@prisma/client'
+
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            name: 'John',
+            email: 'john.doe@example.com',
+            avatarUrl: 'https://github.com/brandaolopes.png'
+        }
+    })
+
+    const pool = await prisma.pool.create({
+        data: {
+            title: 'Bolão de teste',
+            code: 'BOL123',
+            ownerId: user.id,
+
+            participants: {
+                create: {
+                    userId: user.id
+                }
+            }
+        },
+
+        
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-24T16:00:00.201Z',
+            firstTeamCountryCode: 'DE',
+            secondTeamCountryCode: 'BR',
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-28T13:00:00.201Z',
+            firstTeamCountryCode: 'SW',
+            secondTeamCountryCode: 'BR',
+
+            guesses: {
+                create: {
+                    firstTeamPoints: 3,
+                    secondTeamPoints: 1,
+                    
+                    participant: {
+                        connect: {
+                            userId_poolId: {
+                                userId: user.id,
+                                poolId: pool.id,
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
+main()
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import { PrismaClient } from "@prisma/client";
 import cors from '@fastify/cors';
+import Zod from 'zod';
+import ShortUniqueId from 'short-unique-id';
 
 const prisma = new PrismaClient({
     log: ['query'],
@@ -20,6 +22,37 @@ async function bootstap() {
     fastify.get('/pools/count', async () => {
        const count = await prisma.pool.count()
         return {count}
+    })
+
+    fastify.get('/users/count', async () => {
+        const count = await prisma.user.count()
+         return {count}
+     })
+
+     fastify.get('/guesses/count', async () => {
+        const count = await prisma.guess.count()
+         return {count}
+     })
+
+    fastify.post('/pools', async (request, reply) => {
+
+        const createPoolBody = Zod.object({
+            title: Zod.string(),
+        })
+        const { title } = createPoolBody.parse(request.body)
+
+        const generateCode = new ShortUniqueId({ length: 6 })
+        const code = String(generateCode()).toUpperCase()
+
+        await prisma.pool.create({
+            data: {
+                title,
+                code, 
+            }
+        })
+
+        return reply.status(201).send({ code })
+        
     })
 
     await fastify.listen({ port: 3333, /* host: '0.0.0.0' */ })
